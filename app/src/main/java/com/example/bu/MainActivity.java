4a049e9b4f1767014b0a1f0cff40fd1febb959c0
kkyun0823 <kkyun0823@naver.com>
2020-06-08 18:14:40 +0900

    add intent activity

--- app/src/main/java/com/example/bu/MainActivity.java
+++ app/src/main/java/com/example/bu/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.bu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    static final int GET_STRING = 1;
+    EditText text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = (EditText) findViewById(R.id.idText);
+
+    }
+
+    public void clickedSignup(View view){
+        Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+        startActivityForResult(intent, 1);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == GET_STRING){
+            if(resultCode == RESULT_OK){
+
+            }
+        }
     }
 }
